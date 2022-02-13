8241dffd0613ec810346e9e50cb0a753e5a6255a
Jestin Mounts <jkmounts@gmail.com>
2022-02-13 10:14:11 -0500

    feature: add pokemon List

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,55 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Pokedex</h1>
+  <p>Filter Text : {{ filterText }}</p>
+  <input type="text" v-model="filterText" />
+  <ul>
+    <li
+      v-for="(pokemon, index) in pokemonStore.filteredList"
+      :key="`poke-${index}`"
+    >#{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}</li>
+  </ul>
 </template>
+
+<script setup>
+import { reactive, ref, computed, onMounted } from 'vue';
+
+const pokemonList = ref([]);
+const filterText = ref('');
+
+const pokemonStore = reactive({
+  list: [],
+  filteredList: computed(() => {
+  return pokemonStore.list.filter(pokemon => {
+    return pokemon.pokemon_species.name.includes(filterText.value);
+  })
+})
+})
+
+onMounted(async () => {
+  const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+    .then(response => response.json());
+
+  pokemonStore.list = pokeData.pokemon_entries;
+})
+
+
+// export default {
+//   data() {
+//     return {
+//       pokemonList: [],
+//     }
+//   },
+//   async mounted () {
+//     const pokeData = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+//     .then(response => response.json());
+//     console.log({ pokeData });
+
+//     this.pokemonList = pokeData.pokemon_entries
+//   },
+// }
+</script>
+
 
 <style>
 #app {
